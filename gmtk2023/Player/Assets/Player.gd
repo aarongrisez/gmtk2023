2a424a55221eadb017b09b348c1ec7f74396baf5
Aaron Grisez <acgrisez@gmail.com>
2023-07-08 12:32:29 -0700

    moving toward destination working

--- gmtk2023/Player/Assets/Player.gd
+++ gmtk2023/Player/Assets/Player.gd
@@ -4,6 +4,12 @@ onready var rot_tween = $RotTween
 onready var stween = $STween
 onready var sprite = $Sprite
 onready var audio = $AudioStreamPlayer
+onready var sensors = $sensors
+
+
+onready var _blocker_detection_direction = $sensors/lower.cast_to.x
+onready var _edge_detection_position = $sensors/edge.position.x
+
 var tilemap = null
 
 export var speed = 200
@@ -23,6 +29,7 @@ onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * 
 onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+var _last_movement = Vector2()
 var DEFAULT_GRAVITY = Vector2(0,1).rotated(deg2rad(0)) #TODO Make Const
 var gravity_influence = {}
 var gravity_normal = DEFAULT_GRAVITY
@@ -49,6 +56,9 @@ enum STATES {Idle, Walk, Jump, Fall, Climb, Glide}
 var state = STATES.Idle
 var last_state = state
 
+func get_jump_max_reach():
+	return 60
+
 func _ready():
 	tilemap = get_tree().get_current_scene().find_node("WorldMap")
 
@@ -56,6 +66,12 @@ func _process(_delta):
 	get_input()
 	$Label.text = STATES.keys()[state]# + " -- " + str(rotation_degrees)
 	update()
+	if is_facing_edge():
+		print("edge")
+	if is_facing_impossible_blocker():
+		print("impossible")
+	if is_facing_jumpable_blocker():
+		print("jumpable")
 
 func _draw():
 	var pen = get_tree().get_current_scene().find_node("Pen")
@@ -79,8 +95,8 @@ func _draw():
 		var pen_origin = pen.get_global_transform().get_origin() / get_viewport().get_canvas_transform().get_scale()
 		var ray_origin = canvas_origin + pen_origin
 		ray_origin -= tip_offset
-		var laser_target_origin = canvas_origin + laser_target.get_global_transform().get_origin() / get_viewport().get_canvas_transform().get_scale()
-		draw_line(to_local(laser_target_origin), to_local(ray_origin), Color(1, 0, 0), 2, true)
+		#var laser_target_origin = canvas_origin + laser_target.get_global_transform().get_origin() / get_viewport().get_canvas_transform().get_scale()
+		draw_line(Vector2(0,0), to_local(ray_origin), Color(1, 0, 0), 2, true)
 
 func _physics_process(delta):
 	last_state = state
@@ -194,13 +210,13 @@ func _physics_process(delta):
 	#Move and Slide
 	calculate_gravity_normal()
 	var grav_rot = gravity_normal.angle_to(Vector2(0,1))
-	
+	#print("velocity ", velocity)
 	velocity = move_and_slide_with_snap(velocity.rotated(-grav_rot),gravity_normal*snap,-gravity_normal,true,4,deg2rad(80)).rotated(grav_rot)
 	#velocity = move_and_slide_with_snap(velocity,Vector2(0,snap),Vector2.UP)
 
 func get_input(): 
-	movement_dir = Input.get_vector("left","right","null","null").x
-	
+	#movement_dir = Input.get_vector("left","right","null","null").x
+	#print(movement_dir)
 	if Input.is_action_just_pressed("left") || Input.is_action_just_pressed("right") :
 		# -90 bis 90 normal
 		if rotation_degrees > 92 || (rotation_degrees < -92 && rotation_degrees > -200):
@@ -263,6 +279,28 @@ func calculate_wind() -> Vector2:
 	
 	return wind_vec
 
+func _turn_right():
+	sprite.flip_h = false
+	$sensors/lower.cast_to.x = _blocker_detection_direction
+	$sensors/upper.cast_to.x = _blocker_detection_direction
+	$sensors/edge.position.x = _edge_detection_position
+
+func _turn_left():
+	sprite.flip_h = true
+	$sensors/lower.cast_to.x = _blocker_detection_direction * -1
+	$sensors/upper.cast_to.x = _blocker_detection_direction * -1
+	$sensors/edge.position.x = _edge_detection_position * -1
+	$sensors/edge.position.x = _edge_detection_position * -1
+
+func is_facing_jumpable_blocker():
+	return $sensors/lower.is_colliding() and not $sensors/upper.is_colliding()
+
+func is_facing_impossible_blocker():
+	return $sensors/lower.is_colliding() and $sensors/upper.is_colliding()
+
+func is_facing_edge():
+		return not $sensors/edge.is_colliding() and is_on_floor()
+
 func calculate_sprite():
 	sprite.playing = true
 	sprite.speed_scale = 1
@@ -271,9 +309,9 @@ func calculate_sprite():
 	rotation_degrees = n_rot
 	
 	if movement_dir < 0:
-		sprite.flip_h = true
+		_turn_left()
 	elif movement_dir > 0:
-		sprite.flip_h = false
+		_turn_right()
 	
 	if state == STATES.Idle:
 		sprite.animation = "idle"
@@ -397,3 +435,22 @@ func teleport(to: Vector2):
 
 func is_player():
 	return true
+
+func move(move):
+	_last_movement = move
+
+# returns true when reaches position
+func move_to_position(target):
+	var y_distance = abs(position.y -  target.y)
+	var direction = position.direction_to(target)
+	direction.y = 0
+	#print("moving in direction ", direction)
+	if direction.x > 0:
+		movement_dir = 1
+	else:
+		movement_dir = -1
+	if abs(direction.x) < 0.5 and y_distance < 10:
+		return true
+	else:
+		#velocity = direction
+		return false
---
+++ gmtk2023/World/AutoNav.gd
@@ -0,0 +1,102 @@
+extends Navigation2D
+
+onready var player = get_tree().get_current_scene().find_node("Player")
+onready var destination = get_tree().get_current_scene().find_node("Destination")
+
+var _current_path = []
+var _platform_path = []
+
+func _process(_delta):
+	_follow_along_path()
+
+func _update_debug_line():
+	$DEBUG_path_line.clear_points()
+	$DEBUG_path_line.add_point(player.position)
+	for p in _current_path:
+		$DEBUG_path_line.add_point(p)
+
+# Check if all ascending sections have Y distance reachable by the character's jump
+func _is_path_reachable(path):
+	var previous = player.position
+	for current in path:
+		if previous.y > current.y and abs(previous.y - current.y) > player.get_jump_max_reach():
+			return false
+		previous = current
+	return true
+	
+# Calculates an alternative path offsetting path start position
+# it returns de closest valid path or empty array if not option available
+func _calculate_alternate_path():
+	var offset = Vector2(20, 0)
+	var alt_1 = get_simple_path(player.position - offset, destination.position, true)
+	var alt_2 = get_simple_path(player.position + offset, destination.position, true)
+
+	var is_alt_1_reachable = _is_path_reachable(alt_1)
+	var is_alt_2_reachable = _is_path_reachable(alt_2)
+
+	if not (is_alt_1_reachable or is_alt_2_reachable):
+		return []
+
+	if is_alt_1_reachable and not is_alt_2_reachable:
+		return alt_1
+
+	if not is_alt_1_reachable and is_alt_2_reachable:
+		return alt_2
+
+	if alt_1[0].distance_to(destination.position) < alt_2[0].distance_to(destination.position):
+		return alt_1
+
+	return alt_2
+
+# The player should jump in the following scenarios:
+# - There is a blocker ahead.
+# - It reaches a platform's edge, and its next step is at same level or above its current position
+# - It's positioned just bellow it's target, and the target can be reached by jumping
+func _should_jump(next_step):
+	var y_distance = abs(player.position.y -  next_step.y)
+
+	return (
+			player.is_facing_jumpable_blocker() or
+			(player.is_facing_edge() and player.position.y > next_step.y) or
+			(y_distance < player.get_jump_max_reach() and player.position.y - 10 > next_step.y and abs(player.position.x - next_step.x) < 4)
+			)
+
+
+func _follow_along_path():
+	if _current_path.size() == 0:
+		return
+
+	var next_step = _current_path[0]
+	$DEBUG_next_step_marker.rect_position = next_step
+
+	# Before stopping following path, I make sure the character is not jumping,
+	# otherwise it could stop mid-jump, causing it to fall from a platform
+	if player.position.distance_to(destination.position) < 24 and player.is_on_floor():
+		_current_path = []
+		return
+
+	if _should_jump(next_step):
+		player.jump = true
+
+	# remove point from path when reached
+	if player.move_to_position(next_step):
+		_current_path.remove(0)
+		_update_debug_line()
+
+
+# This method is called every 0.4 seconds by PathCalculationTimer node
+func _on_PathCalculationTimer_timeout():
+	var path = get_simple_path(player.position, destination.position)
+	# if path has unreachable sections, calculate alternate paths
+	# A section is considered unreachable when the Y distance between two points
+	# is higher than the character's jump can reach.
+	if not _is_path_reachable(path):
+		#print("unreachable, calculating alternative")
+		path = _calculate_alternate_path()
+	_current_path = path
+
+	# Removing first point as it will always be the NPC current position.
+	if path.size() > 0:
+		_current_path.remove(0)
+
+	_update_debug_line()
