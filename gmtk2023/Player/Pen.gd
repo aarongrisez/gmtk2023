59df193d291b2c8749c14dbeea618323e081b6e4
Aaron Grisez <acgrisez@gmail.com>
2023-07-07 21:03:52 -0700

    fucking pen rotation hack

--- gmtk2023/Player/Pen.gd
+++ gmtk2023/Player/Pen.gd
@@ -5,10 +5,10 @@ extends Sprite
 # var a = 2
 # var b = "text"
 
-
+onready var target = get_node("../LaserTarget")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	look_at(get_global_mouse_position())
+	look_at(target.global_position)
