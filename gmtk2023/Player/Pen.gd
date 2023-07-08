338e60451a36a39b1b21ad081fe641c503fb47fd
Aaron Grisez <acgrisez@gmail.com>
2023-07-07 23:48:13 -0700

    Simplify pen rotation mechanic

--- gmtk2023/Player/Pen.gd
+++ gmtk2023/Player/Pen.gd
@@ -1,14 +1,21 @@
 extends Sprite
 
+var MIN_ROTATION = -1.5777
+var MAX_ROTATION = 1.8888
+var speed = 250
+export var turning = 4.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-onready var target = get_node("../LaserTarget")
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	look_at(target.global_position)
+	var new_rotation = 0
+	var current_rotation = get_rotation()
+	if Input.is_action_pressed('ui_left'):
+		if current_rotation < MAX_ROTATION:
+			rotation += turning * delta
+	if Input.is_action_pressed('ui_right'):
+		if current_rotation > MIN_ROTATION:
+			rotation -= turning * delta
+
+	rotate(new_rotation)
