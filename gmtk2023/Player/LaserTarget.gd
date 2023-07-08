8fd231399c8713e7f7aa8617277fecb7be63e970
Aaron Grisez <acgrisez@gmail.com>
2023-07-07 22:11:42 -0700

    cosmetic improvements, CI fix

--- gmtk2023/Player/LaserTarget.gd
+++ gmtk2023/Player/LaserTarget.gd
@@ -1,17 +1,17 @@
 extends KinematicBody2D
 
-var speed = 250
+var speed = 2500
 var velocity = Vector2()
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed
 	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
-		velocity.y += 1
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x += 1
 		velocity.y -= 1
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+		velocity.y += 1
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
